13a8645fca0ad2bf712437ae9a1c2c8b661fcd58
Abhy.Kizhakkepat <Abhy.Kizhakkepat@tattsgroup.com>
2019-07-30 17:00:26 +1000

    Updates

--- MyASPDotNetCoreApp/MyASPDotNetCoreApp/HelloWorldController.cs
+++ MyASPDotNetCoreApp/MyASPDotNetCoreApp/HelloWorldController.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Text.Encodings.Web;
+using MyASPDotNetCoreApp.Models;
 
 namespace MyASPDotNetCoreApp
 {
@@ -17,6 +18,29 @@ namespace MyASPDotNetCoreApp
         public string Xamarin(string name)
         {
             return HtmlEncoder.Default.Encode($"Hello {name}, welcome to Visual Studio for Mac");
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult ScanTicket()
+        {
+            ViewData["Message"] = "Scan the Ticket";
+
+            var viewModel = new Address()
+            {
+                Name = "Microsoft",
+                Street = "One Microsoft Way",
+                City = "Redmond",
+                State = "WA",
+                PostalCode = "98052-6399"
+            };
+
+            return View(viewModel);
         }
     }
 }
