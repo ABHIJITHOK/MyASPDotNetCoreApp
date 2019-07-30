80a78360c5ac299db5ce3e57e933856c1837db7d
Abhy.Kizhakkepat <Abhy.Kizhakkepat@tattsgroup.com>
2019-07-30 11:52:22 +1000

    Basic ASP.NET Core Application

---
+++ MyASPDotNetCoreApp/MyASPDotNetCoreApp/HelloWorldController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Text.Encodings.Web;
+
+namespace MyASPDotNetCoreApp
+{
+    public class HelloWorldController : Controller
+    {
+        public string Index()
+        {
+            return "This is my default action";
+        }
+
+        public string Xamarin(string name)
+        {
+            return HtmlEncoder.Default.Encode($"Hello {name}, welcome to Visual Studio for Mac");
+        }
+    }
+}
